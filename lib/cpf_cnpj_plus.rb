ca5109532848d9b68dd4b3238f8a5a95228c5912
DougNeo <doug.neo@gmail.com>
2025-07-01 23:28:43 -0300

    Add two principal command

--- lib/cpf_cnpj_plus.rb
+++ lib/cpf_cnpj_plus.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 require_relative "cpf_cnpj_plus/version"
+require_relative "cpf_cnpj_plus/validator"
 
 module CpfCnpjPlus
   class Error < StandardError; end
-  # Your code goes here...
+  def self.valid_cnpj?(cnpj)
+    Validator.valid_cnpj?(cnpj)
+  end
+
+  def self.valid_cpf?(cpf)
+    Validator.valid_cpf?(cpf)
+  end
 end
